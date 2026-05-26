0f2d18c7d92d2cf230fbc229653ff1f991f33395
onlychen120 <chennanxing@proton.me>
2026-05-26 13:43:43 +0200

    autolist

---
+++ dot_config/nvim/lua/conf/plugins/autolist.lua
@@ -0,0 +1,23 @@
+return {
+	"gaoDean/autolist.nvim",
+	ft = { "markdown", "text" },
+	config = function()
+		require("autolist").setup()
+
+		-- Automatically add list items on Enter in insert mode
+		vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+
+		-- Automatically add list items with 'o' and 'O' in normal mode
+		vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+		vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+
+		-- Recalculate numbered lists when you indent/dedent or delete
+		vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+		vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+		vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+		vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+
+		-- Optional: Toggle checkboxes with <CR> in normal mode
+		vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+	end,
+}
